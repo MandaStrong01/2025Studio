86660b78dd5232972661bdcd95f3f4df08fab4cc
MandaStrong01 <woolleya129@gmail.com>
2025-12-29 23:46:27 -0500

    Updated Page11.tsx

--- src/pages/Page11.tsx
+++ src/pages/Page11.tsx
@@ -1,4 +1,4 @@
-import VideoPlayer from "../components/VideoPlayer";
+import VideoStudio from "./VideoStudio";
 
 export default function Page11() {
   return (
@@ -7,14 +7,9 @@ export default function Page11() {
         backgroundColor: "#000",
         color: "#fff",
         minHeight: "100vh",
-        padding: "24px",
       }}
     >
-      <h1 style={{ marginBottom: "16px" }}>
-        Page 11 – Movie Preview
-      </h1>
-
-      <VideoPlayer />
+      <VideoStudio />
     </div>
   );
 }
